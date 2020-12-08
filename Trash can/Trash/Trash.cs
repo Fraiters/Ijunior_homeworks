cb96268fa7e4797be53234259bb3619d1a0cda05
Fraiters <67577747+Fraiters@users.noreply.github.com>
2020-12-08 15:48:01 +0300

    Update Trash.cs

--- Trash can/Trash/Trash.cs
+++ Trash can/Trash/Trash.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,7 +6,7 @@ public class Trash : MonoBehaviour
 { 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.TryGetComponent(out Rigidbody rigidbody))
+        if (collision.gameObject.TryGetComponent(out Transform transform))
         {
             Destroy(collision.gameObject);
         }
