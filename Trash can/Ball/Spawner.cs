c4823eabf7ae761f3d1c6820725c0b413b87e58d
Fraiters <67577747+Fraiters@users.noreply.github.com>
2020-12-08 13:14:13 +0300

    Add files via upload

---
+++ Trash can/Ball/Spawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _template;
+    [SerializeField] private Transform[] _spawnPoints;
+
+    private void Start()
+    {
+        SpawnElements(_template, _spawnPoints);
+    }
+
+    private void SpawnElements(GameObject template, Transform[] spawnPoints)
+    {
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            Instantiate(template, spawnPoints[i].position, Quaternion.identity);
+        }
+        
+    }
+}
