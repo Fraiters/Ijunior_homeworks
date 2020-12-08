55b3cb23d455d8d148d2f6956753b9968cd59673
Fraiters <67577747+Fraiters@users.noreply.github.com>
2020-12-08 12:13:02 +0300

    Add files via upload

---
+++ Trash/Trash.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trash : MonoBehaviour
+{ 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.TryGetComponent(out Rigidbody rigidbody))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
