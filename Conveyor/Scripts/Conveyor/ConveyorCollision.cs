2eafb7c697e29da3c47fde3e8d643548d5a52d73
Fraiters <67577747+Fraiters@users.noreply.github.com>
2020-12-14 10:57:24 +0300

    Add files via upload

---
+++ Conveyor/Scripts/Conveyor/ConveyorCollision.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ConveyorCollision : MonoBehaviour
+{
+    [SerializeField] private int _force;
+    private void OnCollisionEnter(Collision collision)
+    {
+        collision.rigidbody.AddForce(Vector3.forward * _force, ForceMode.Impulse);
+    }
+}
