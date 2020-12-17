c5afc910eda374d51321a760927afdd436a6d164
Fraiters <67577747+Fraiters@users.noreply.github.com>
2020-12-17 22:40:36 +0300

    Add files via upload

---
+++ Bowling/Scripts/Ball/BallSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour
+{
+    [SerializeField] private Transform[] _spawnPoint;
+    [SerializeField] private GameObject _template;
+
+    private void Start()
+    {
+        SpawnBall(_template, _spawnPoint);
+    }
+
+    private void SpawnBall(GameObject template, Transform[] spawnPoint)
+    {
+        for (int i = 0; i < spawnPoint.Length; i++)
+        {
+            Instantiate(template, spawnPoint[i].position, Quaternion.identity);
+        }
+    }
+}
---
+++ Bowling/Scripts/Conveyor/ConveyorCollision.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ConveyorCollision : MonoBehaviour
+{
+    [SerializeField] private int _force;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.TryGetComponent(out Ball ball))
+        {
+            collision.rigidbody.AddForce(Vector3.forward * _force, ForceMode.Impulse);
+        }
+        
+    }
+}
